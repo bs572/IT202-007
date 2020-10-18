cfafeaa00a9553b6478fbcd85fc3d3e36f2838cd
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-10-18 11:44:09 -0400

    updated Profile

--- Project/profile.php
+++ Project/profile.php
@@ -75,7 +75,7 @@ if (isset($_POST["saved"])) {
         }
         //password is optional, so check if it's even set
         //if so, then check if it's a valid reset request
-        if (!empty($_POST["password"]) && !empty($_POST["confirm"])) {
+        if (!empty($_POST["newPassword"]) && !empty($_POST["confirm"])) {
             $stmt = $db->prepare("SELECT password from Users WHERE id = :id");
             $params = array(":id" => $userID);
             $r = $stmt->execute($params);
