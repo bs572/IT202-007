0d771aed17f0276ee4cf6bccf36b4d3396b0db37
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-10-19 21:03:14 -0400

    testing dual purpose login

--- Project/login.php
+++ Project/login.php
@@ -22,15 +22,15 @@ if (isset($_POST["login"])) {
         $isValid = false;
         flash("Email or password missing"); }
 
-    if (!strpos($email, "@")) {
+    /*if (!strpos($email, "@")) {
         $isValid = false;
         //echo "<br>Invalid email<br>";
-        flash("Invalid email"); }
+        flash("Invalid email"); } */
 
     if ($isValid) {
         $db = getDB();
         if (isset($db)) {
-            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email LIMIT 1");
+            $stmt = $db->prepare("SELECT id, email, username, password from Users WHERE email = :email OR username = :username LIMIT 1");
 
             $params = array(":email" => $email);
             $r = $stmt->execute($params);
