738ddf7f5e1366dacf816cb39c4a716b42027a28
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-11-12 18:26:33 -0500

    yeet

--- Project/test_edit_cart.php
+++ Project/test_edit_cart.php
@@ -63,7 +63,7 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <label>Product</label>
         <select name="id" value="<?php echo $result["id"];?>" >
             <option value="-1">None</option>
-            <?php foreach ($product as $products): ?>
+            <?php foreach ($products as $product): ?>
                 <option value="<?php safer_echo($result["product_id"]); ?>" <?php echo ($result["product_id"] == $products["id"] ? 'selected="selected"' : ''); ?>
                 ><?php safer_echo($products["name"]); ?></option>
             <?php endforeach; ?>
