9ed194b993deea4ce96b3f8b026a60436a213207
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-11-12 15:06:50 -0500

    yeet

--- Project/test_list_cart.php
+++ Project/test_list_cart.php
@@ -15,7 +15,10 @@ if (isset($_POST["query"])) {
 if (isset($_POST["search"]) && !empty($query)) {
     $db = getDB();
     $stmt = $db->prepare("SELECT product_id From Cart join Users on Cart.user_id = Products.id where Cart.user_id=:user_id and Products.name like :q LIMIT 10");
-    $r = $stmt->execute([":q" => "%$query%"]);
+    $r = $stmt->execute([
+        ":q" => "%$query%",
+        ":user_id"=> $id,
+        ]);
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
