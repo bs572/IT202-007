23bb2345b6d3c3dde10efb76885948bee119ef54
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-11-12 20:00:35 -0500

    yeet

--- Project/test_list_products.php
+++ Project/test_list_products.php
@@ -46,6 +46,10 @@ if (isset($_POST["search"]) && !empty($query)) {
                         <div><?php safer_echo($r["quantity"]); ?></div>
                     </div>
                     <div>
+                        <div>Description:</div>
+                        <div><?php safer_echo($r["description"]); ?></div>
+                    </div>
+                    <div>
                         <a type="button" href="test_edit_products.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
                         <a type="button" href="test_view_products.php?id=<?php safer_echo($r['id']); ?>">View</a>
                     </div>
