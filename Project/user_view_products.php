50fe74365db904643e89fdcb237baf131efb4dae
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-11-16 18:53:27 -0500

    added user view

---
+++ Project/user_view_products.php
@@ -0,0 +1,47 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+//we'll put this at the top so both php block have access to it
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+?>
+<?php
+//fetching
+$result = [];
+if (isset($id)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT name, price, quantity, description, user_id, Users.username FROM Products JOIN Users on Products.user_id = Users.id where Products.id = :id");
+    $r = $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$result) {
+        $e = $stmt->errorInfo();
+        flash($e[2]);
+    }
+}
+?>
+<?php if (isset($result) && !empty($result)): ?>
+    <div class="card" style="width: 18rem;">
+  <img src="https://www.precisionturbo.net/images/products/small/583-1.jpg" class="card-img-top" alt="CEA 7285">
+  <div class="card-body">
+    <h5 class="card-title">Precision 7285</h5>
+    <p class="card-text">Gen2 Precision 7285 class legal turbo for SportFWD, and World Cup Finals Super Street</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    
+        <div class="card-title">
+            <?php safer_echo($result["name"]); ?>
+        </div>
+        <div class="card-body">
+            <div>
+                <p>Stats</p>
+                <div>Price: <?php safer_echo($result["price"]); ?></div>
+                <div>Quantity: <?php safer_echo($result["quantity"]); ?></div>
+                <div>Description <?php safer_echo($result["description"]); ?></div>
+            </div>
+        </div>
+    </div>
+<?php else: ?>
+    <p>Error looking up id...</p>
+<?php endif; ?>
+<?php require(__DIR__ . "/partials/flash.php");
