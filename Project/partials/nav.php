60425ac318220a1ccf2cdcf75cebe9ee4cba6ca0
bs572@njit.edu <bryonscott@Bryons-MBP.fios-router.home>
2020-11-18 11:52:44 -0500

    updated nav, removed second CSS

--- Project/partials/nav.php
+++ Project/partials/nav.php
@@ -18,8 +18,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="register.php">Register</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
-            <li><a href="profile.php">Profile</a></li>
-            <li><a href="logout.php">Logout</a></li>
+            <li><a href="<?php echo getURL("profile.php");?>">Profile</a></li>
+            <li><a href="<?php echo getURL("logout.php");?>">Logout</a></li>
         <?php endif; ?>
         <?php if (has_role("Admin")): ?>
             <li><a href="<?php echo getURL("Admin/Products/create_products.php");?>">Create Product</a></li>
