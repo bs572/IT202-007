a8d0ebbae65696cd25c74f3be457a8e437238149
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-11-19 15:25:35 -0500

    updated user cart

--- Project/user_cart.php
+++ Project/user_cart.php
@@ -1,4 +1,12 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
+
+<?php
+if (!is_logged_in()) {
+    flash("You must be logged in to access this page");
+    die(header("Location: ../../login.php"));
+}
+?>
+
 <?php
 $query = "";
 $id=get_user_id();
