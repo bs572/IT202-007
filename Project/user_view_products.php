eb9c935285298677fb81b09e66cb7ac07b5bd4f3
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-11-20 19:15:55 -0500

    updated user view products

--- Project/user_view_products.php
+++ Project/user_view_products.php
@@ -1,6 +1,3 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<?php
-//we'll put this at the top so both php block have access to it
 if (isset($_GET["id"])) {
     $id = $_GET["id"];
 }
@@ -20,44 +17,20 @@ if (isset($id)) {
 }
 ?>
 <?php if (isset($result) && !empty($result)): ?>
-    <div class="card" style="width: 18rem;">
-  <img src="https://www.precisionturbo.net/images/products/small/583-1.jpg" class="card-img-top" alt="CEA 7285">
-  <div class="card-body">
-    <h5 class="card-title">Precision 7285</h5>
-    <p class="card-text">Gen2 Precision 7285 class legal turbo for SportFWD, and World Cup Finals Super Street</p>
-    <form method="POST">
-    <input type="hidden" name="productID" value="<?php echo $result["product_id"];?>"/>
-    <input type="submit" value="Add to Cart"/>
-    </form>
-  </div>
-
+    <div class="card">
+        <div class="card-title">
+            <?php safer_echo($result["name"]); ?>
+        </div>
+        <div class="card-body">
+            <div>
+                <p>Stats</p>
+                <div>Price: <?php safer_echo($result["price"]); ?></div>
+                <div>Quantity: <?php safer_echo($result["quantity"]); ?></div>
+                <div>Description <?php safer_echo($result["description"]); ?></div>
+            </div>
+        </div>
+    </div>
 <?php else: ?>
     <p>Error looking up id...</p>
 <?php endif; ?>
-<?php 
-if (isset($_POST["Add To Cart"])) {
-    $id = $_POST["product_id"];
-    //TODO add proper validation/checks
-    $pr = $_POST["price"];
-	$quantity = 1;
-    $user = get_user_id();
-    $db = getDB();
-    $stmt = $db->prepare("SELECT FROM Products(price) VALUES(:pr)");
-    $stmt = $db->prepare("INSERT INTO Cart (product_id, price, quantity,user_id) VALUES(:id, :pr, :quantity, :user)");
-    $r = $stmt->execute([
-        ":id"=>$id,
-        ":pr"=>$pr,
-		":quantity"=>$quantity,
-		":user"=>$user
-    ]);
-    if ($r) {
-        flash("Created successfully with id: " . $db->lastInsertId());
-    }
-    else {
-        $e = $stmt->errorInfo();
-        flash("Error creating: " . var_export($e, true));
-    }
-}
-?>
-
-<?php require(__DIR__ . "/partials/flash.php");
+<?php require(__DIR__ . "/../../partials/flash.php");
