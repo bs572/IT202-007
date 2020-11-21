2b4529421957b9399b661fe6aa5c10f0a7faf4ee
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-11-20 19:18:16 -0500

    updated user view products

--- Project/user_view_products.php
+++ Project/user_view_products.php
@@ -35,4 +35,4 @@ if (isset($id)) {
 <?php else: ?>
     <p>Error looking up id...</p>
 <?php endif; ?>
-<?php require(__DIR__ . "/../../partials/flash.php");
+<?php require(__DIR__ . "/partials/flash.php");
