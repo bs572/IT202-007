ed9d2919ab6ac72927c11d547f9f0dc0c84c4bc0
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-11-22 18:46:56 -0500

    updated user list

--- Project/user_list_products.php
+++ Project/user_list_products.php
@@ -3,7 +3,7 @@
 $query = "";
 $results = [];
 $selectedCat = '';
-$dbQuery = "SELECT name, id, price, category, quantity, description, visibility, user_id from Products WHERE 1 = 1";
+$dbQuery = "SELECT name, id, price, category, quantity, description, visibility, user_id from Products WHERE 1 = 1 AND visibility = 1";
 $params = [];
 if (isset($_POST["query"])) {
     $query = $_POST["query"];
