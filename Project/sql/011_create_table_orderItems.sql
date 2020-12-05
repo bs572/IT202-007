57528781c3d3e7e60f594a866c84d75cd297b2da
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-12-05 12:26:44 -0500

    added orders and orderItems files

---
+++ Project/sql/011_create_table_orderItems.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS 'OrderItems'
+(
+    `id`         int auto_increment not null,
+    `product_id`    int,
+    `order_id` int,
+    `created`    timestamp default current_timestamp,
+    `quantity` int,
+    `unit_price` decimal(12, 2),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`product_id`) REFERENCES Users(`id`),
+    FOREIGN KEY (`order_id`) REFERENCES Orders(`id`),
+)
