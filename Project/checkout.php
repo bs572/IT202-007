903acf970249b8785b5fbb8789615da772394eba
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-12-05 13:19:55 -0500

    updated checkout

--- Project/checkout.php
+++ Project/checkout.php
@@ -95,8 +95,8 @@ $db = getDB();
             <select name="id" value="<?php echo $payment["payment_method"];?>" >
                 <option value="-1">None</option>
                 <?php foreach ($payments as $payment): ?>
-                    <option value="<?php safer_echo($payment["payment_method"]); ?>" 
-                    ><?php safer_echo($product["name"]); ?></option>
+                    <option value="<?php safer_echo($payment["payment_method"]); ?>" <?php echo ($result["product_id"] == $product["id"] ? 'selected="selected"' : ''); ?>
+                    ><?php safer_echo($payment["payment_method"]); ?></option>
                 <?php endforeach; ?>
             </select>
             <?php endif; ?>
