dd2070bd5a9d056bf2382baed737d145d1ea5667
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-12-05 16:03:25 -0500

    updated order tables

--- Project/sql/010_create_table_orders.sql
+++ Project/sql/010_create_table_orders.sql
@@ -1,16 +1,12 @@
 CREATE TABLE IF NOT EXISTS `Orders` (
 	`id` int auto_increment,
-	`product_id` int,
 	`user_id` int,
-	`quantity` int,
 	`total_price` decimal(12, 2),
-	`orderRef` int, -- this will be a manually handled id to group order items together
     `payment_method` varchar(20),
     `address` varchar(120),
     `modified`    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP on update current_timestamp,
     `created`     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
 	primary key (id),
 	foreign key (user_id) references Users(id),
-	foreign key (product_id) references Products(id)
 
 )
