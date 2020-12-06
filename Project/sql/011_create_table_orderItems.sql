4180b9f43e4149be8552a1d8b0ec74166e7a8e66
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-12-06 16:44:39 -0500

    Updated 011 sql file.....again.

--- Project/sql/011_create_table_orderItems.sql
+++ Project/sql/011_create_table_orderItems.sql
@@ -6,7 +6,9 @@ CREATE TABLE IF NOT EXISTS `OrderItems`
     `created`    timestamp default current_timestamp,
     `quantity` int,
     `unit_price` decimal(12, 2),
+    `user_id` int,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`product_id`) REFERENCES Users(`id`),
+    FOREIGN KEY (`product_id`) REFERENCES Products(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
     FOREIGN KEY (`order_id`) REFERENCES Orders(`id`)
 )
