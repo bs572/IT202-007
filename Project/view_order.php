331a51fb4d887e330e33a248641bc4afd22a1a51
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-12-06 21:47:33 -0500

    updated view order:

--- Project/view_order.php
+++ Project/view_order.php
@@ -24,7 +24,7 @@ if (isset($_POST["query"])) {
 ?>
 <?php   
     $db = getDB();
-    $stmt = $db->prepare("SELECT id, product_id, unit_price, quantity, name From OrderItems JOIN Products Where order_id=:order_id LIMIT 10");
+    $stmt = $db->prepare("SELECT OrderItems.id, order_id, product_id, unit_price, quantity, name From OrderItems JOIN Products Where order_id=:order_id LIMIT 10");
     $r = $stmt->execute([":order_id"=> $orderID,]);
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
