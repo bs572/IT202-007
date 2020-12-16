b9331e57e536ac6d1a15297cc6f564cd5333ab7d
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-12-16 18:59:17 -0500

    updated user list

--- Project/user_list_products.php
+++ Project/user_list_products.php
@@ -1,12 +1,5 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
-if (!has_role("Admin")) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
-    flash("You don't have permission to access this page");
-    die(header("Location: login.php"));
-}
-?>
-<?php
 $page = 1;
 $countOnPage = 10;
 $total = 0;
