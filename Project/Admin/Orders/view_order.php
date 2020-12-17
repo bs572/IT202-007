9cd2370a87624cd61c5ca95792ccc37362f24e6a
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-12-17 17:11:02 -0500

    updated view orders

--- Project/Admin/Orders/view_order.php
+++ Project/Admin/Orders/view_order.php
@@ -1,4 +1,4 @@
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php require_once(__DIR__ . "/../../partials/nav.php"); ?>
 
 <?php
 if (!has_role("Admin")) {
@@ -51,4 +51,4 @@ $results = [];
         <p>You don't have any orders matching the selected criteria</p>
     <?php endif; ?>
 
-<?php require(__DIR__ . "/partials/flash.php");
+<?php require(__DIR__ . "/../../partials/flash.php");
