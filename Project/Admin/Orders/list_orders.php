ac7303f85bc40d425a61e7ac3b4f357153e9ce73
bs572@njit.edu <bryonscott@Bryons-MacBook-Pro.local>
2020-12-17 16:45:01 -0500

    updated admin list orders

--- Project/Admin/Orders/list_orders.php
+++ Project/Admin/Orders/list_orders.php
@@ -29,7 +29,7 @@ $cumulativeTotal = 0;
 $total =0;
 $address = "";
 
-$dataQuery = "SELECT OrderItems.quantity, OrderItems.unit_price, Products.name From OrderItems join Products on Products.id WHERE 1=1";
+$dataQuery = "SELECT OrderItems.quantity, OrderItems.unit_price, OrderItems.order_id, Products.name From OrderItems join Products on Products.id WHERE 1=1";
 $pageQuery = "SELECT COUNT(1) as total from Orders WHERE 1=1";
 
 if(isset($_GET["page"])){
